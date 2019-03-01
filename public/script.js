b2449464d7a910ea473e3774bf8be30cfaeba241
GibsDev <crgibson2@live.com>
2019-03-01 00:08:52 -0500

    first commit

---
+++ public/script.js
@@ -0,0 +1,125 @@
+let list = document.getElementById('list');
+let menu = document.getElementById('menu');
+let input = document.getElementById('input');
+let submit = document.getElementById('submit');
+let events = new EventSource('/events');
+
+function httpget(url, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (callback) {
+                callback(this.responseText);
+            }
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function httppost(object, url, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (callback) {
+                callback(this.responseText);
+            }
+        }
+    };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send(JSON.stringify(object));
+}
+
+function httpdelete(object, url, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (callback) {
+                callback(this.responseText);
+            }
+        }
+    };
+    xhttp.open("DELETE", url, true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send(JSON.stringify(object));
+}
+
+let housemanlist = {
+    _value: [],
+    set value(val) {
+        this._value = val;
+        housemanUpdated();
+    },
+    get value() {
+        return this._value;
+    }
+};
+
+events.addEventListener('housemanlist', (event) => {
+    // Calls the setter for value
+    housemanlist.value = JSON.parse(event.data);
+});
+
+/**
+ * Get the initial state of the houseman list
+ */
+httpget("/list", (data) => {
+    housemanlist.value = JSON.parse(data);
+});
+
+let ITEMS;
+
+httpget("/items", (data) => {
+    ITEMS = JSON.parse(data);
+});
+
+function getMessageById(id) {
+    for (let i = 0; i < housemanlist.value.length; i++) {
+        if (housemanlist.value[i].id == id) {
+            return housemanlist.value[i];
+        }
+    }
+}
+
+function housemanUpdated() {
+    if (housemanlist.value.length == 0) {
+        list.innerHTML = '<div><p>The houseman list is empty</b></div>';
+        return;
+    }
+    list.innerHTML = '';
+    for (let i = 0; i < housemanlist.value.length; i++) {
+        list.innerHTML += `<div><button id=${housemanlist.value[i].id}>X</button><p>${housemanlist.value[i].message}</p></div>`;
+        let div = list.lastChild;
+        let button = div.firstChild;
+    }
+}
+
+list.addEventListener('click', (click) => {
+    console.log('list clicked');
+    console.log(click.target);
+    if (click.target.nodeName.toLowerCase() == 'button') {
+        httpdelete(getMessageById(click.target.id), "/list", (res) => {
+            let resjson = JSON.parse(res);
+            console.log(`${resjson.id} has been deleted`);
+        });
+    }
+});
+
+function Message(message) {
+    return {
+        message: message,
+        time: Date.now(),
+        id: MD5(message + this.time)
+    }
+}
+
+submit.addEventListener('click', () => {
+    if (input.value.length > 0) {
+        httppost(new Message(input.value), '/list');
+    }
+})
+
+function log(str) {
+    console.log(str);
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+
+const fs = require('fs');
+
+var jsonParser = bodyParser.json();
+
+/**
+ * Server side state object for list of requests
+ */
+let housemanlist = [];
+
+const EventEmitter = require('events');
+const events = new EventEmitter();
+
+/**
+ * Allows a client to subscribe to receive an updated list
+ * when something changes.
+ */
+app.get('/events', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    
+    const updatefun = (event, data) => {
+        // Write the event to the /events stream
+        res.write(`event: ${String(event)}\ndata: ${JSON.stringify(data)}\n\n`);
+    }
+    
+    events.addListener('update', updatefun);
+    
+    req.connection.on('close', () => {
+        events.removeListener('update', updatefun);
+        res.end();
+    });
+});
+
+/**
+ * Return a list of all things currently being requested in json
+ * Technically this should on be called once from an ajax request
+ * by the initial openning of the page. The housemanlist should
+ * then be updated using the data from the /events stream.
+ */
+app.get('/list', (req, res) => {
+    res.setHeader('Content-type', 'application/json');
+    res.send(JSON.stringify(housemanlist));
+    res.end();
+});
+
+/**
+ * Return the list of all possible items as json
+ * 
+ * This will be used by the client to gather a list of all
+ * items from the server. There should only be one server side
+ * JSON file that contains all of the items that can be
+ * requested. This request should simply respond by sending
+ * that JSON file back to the requester.
+ */
+app.get('/items', (req, res) => {
+    res.setHeader('Content-type', 'application/json');
+    fs.readFile('items.json', (err, data) => {
+        res.end(data);
+    });
+});
+
+/**
+ * Used to post requests to the houseman list
+ */
+app.post('/list', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    for (let i = 0; i < housemanlist.length; i++) {
+        if (req.body.message == housemanlist[i].message) {
+            res.send('Tried to send duplicate message');
+            return;
+        }
+    }
+    housemanlist.push(req.body);
+    housemanlist.sort((a, b) => {return a.time - b.time});
+    update();
+    res.send(req.body);
+});
+
+app.delete('/list', jsonParser, (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    for (let i = 0; i < housemanlist.length; i++) {
+        if (req.body.id == housemanlist[i].id) {
+            housemanlist.splice(i, 1);
+            break;
+        }
+    }
+    update();
+    res.send(req.body);
+});
+
+/**
+ * Serve client side html code from the client directory
+ */
+app.use(express.static('public'));
+
+/**
+ * Start the server listening on the specified port
+ */
+app.listen(port, () => console.log(`Listeneing on port ${port}`));
+
+/**
+ * Used as a mechanism to set off an event for testing
+ */
+/*
+setInterval(() => {
+    update();
+}, 1000);
+*/
+function update() {
+    events.emit('update', 'housemanlist', housemanlist);
+}
