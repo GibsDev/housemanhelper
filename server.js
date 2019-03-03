86692c6b28d13e3fe192b3a66d8cce439fcb503b
GibsDev <crgibson2@live.com>
2019-03-03 17:10:24 -0500

    using self signed certificate for HTTPS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 // HTTPS
-const port = 443;
+const port = 8080;
 
 /**
  * Parse all JSON into the req.body object
@@ -22,7 +24,15 @@ app.use('/api', housemanapi);
  */
 app.use(express.static('public', { extensions:['html'] }));
 
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}
+
 /**
  * Start the server listening on the specified port
  */
-app.listen(port, () => console.log(`Listeneing on port ${port}`));
+https.createServer(options, app).listen(port, function () {
+        console.log(`Listeneing on port ${port}`)
+    }
+);
