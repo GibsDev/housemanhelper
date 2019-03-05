c03dbc470ea47ed510b45851e5365bb784a7f318
GibsDev <crgibson2@live.com>
2019-03-05 18:51:06 -0500

    start server normally without https and use openshift for https

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
-// HTTPS
-const port = 443;
+const port = 8080;
+// const https = require('https');
 
 /**
  * Parse all JSON into the req.body object
@@ -24,15 +22,20 @@ app.use('/api', housemanapi);
  */
 app.use(express.static('public', { extensions:['html'] }));
 
+/*
 const options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 }
+*/
 
 /**
  * Start the server listening on the specified port
  */
+app.listen(port, () => console.log(`Listeneing on port ${port}`));
+/*
 https.createServer(options, app).listen(port, function () {
         console.log(`Listeneing on port ${port}`)
     }
 );
+*/
